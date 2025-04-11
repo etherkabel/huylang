60ff6f09375026369ff17808168a852cd7afdd12
etherkabel <>
2025-04-11 21:18:20 +0300

    Исправлен форматированный вывод

--- src/print.c
+++ src/print.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #ifndef _HUYLANG_H
 #include "include/huylang.h"
 #endif
-
 
 void print(FILE *f) {
     char buf;
@@ -15,16 +15,23 @@ void print(FILE *f) {
     if ((buf = fgetc(f)) == '$') {
         char var_name[BUFFER_SIZE];
         int j;
-        for (j=0;(buf = fgetc(f)) != ')' && buf != EOF;++j) {
+        bool isVar = false;
+        for (j=0;(buf = fgetc(f)) != ' ' && buf != EOF && buf != ')';++j) {
             var_name[j] = buf;
         }
         var_name[j+1] = '\0';
         for (int j=0;j < VAR_INT_COUNT;++j) {
             if (strcmp(__int[j].name,var_name)) {
-                fprintf(stdout,"%s\n",__int[j].name);
+                fprintf(stdout,"%d\n",__int[j].value);
+                isVar = true;
                 break;
             }
         }
+        if (!isVar) {
+            fprintf(stderr,"Ошибка! Не найдена переменная %s\n",var_name);
+        }
+        isVar = false;
+        return;
     }
     while ((buf = fgetc(f)) != ')' && buf != EOF) {
         printf("%c",buf);
